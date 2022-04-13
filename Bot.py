d546716bf16bdd7f7f857d66e6e33cf02a806db1
Warwarss <96444036+Warwarss@users.noreply.github.com>
2022-04-13 19:17:41 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,9 +1,6 @@
 import os
 import time
 import datetime
-import sys
-reload(sys) 
-sys.setdefaultencoding("utf-8")
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -24,10 +21,10 @@ def send_message_to (name,message):
         chat=driver.find_element(by=By.XPATH, value=chat_value)
         time.sleep(1)
         chat.click()
-        chat_box='//div[@title="Schreib eine Nachricht"]'
+        chat_box=driver.find_element(by=By.XPATH, value='//div[@title="Schreib eine Nachricht"]')
         for i in range(25):
             chat_box.click()
-            chat_box.send_keys(message)
+            chat_box.send_keys("Allah")
             chat_box.send_keys(Keys.RETURN)
 
 PATH="C:\Program Files (x86)\Chromedriver\chromedriver.exe"
@@ -37,20 +34,6 @@ options.add_argument(r"--profile--directory=C:\Users\ACER\AppData\Local\Google\C
 driver = webdriver.Chrome(PATH,chrome_options=options)
 wait5=WebDriverWait(driver, 5)
 driver.get("https://web.whatsapp.com")
-send_message_to("kızılbaş" .encode("unicode"),"Allah")
-search_box=wait5.until(EC.presence_of_element_located((By.XPATH,'//div[@contenteditable="true"][@data-tab="3"]')))
-search_box.clear()
-search_box.click()
-search_box.send_keys(Contact)
-time.sleep(5)
-chat=driver.find_element(by=By.XPATH, value='//span[@title="illegal ama kutsal"]')
-time.sleep(1)
-chat.click()
-time.sleep(1)
-chat_box=driver.find_element(by=By.XPATH, value='//div[@title="Schreib eine Nachricht"]')
-for i in range(25):
-    chat_box.click()
-    chat_box.send_keys("Allah")
-    chat_box.send_keys(Keys.RETURN)
+send_message_to("kizilbas","Allah")
 
 time.sleep(10)
